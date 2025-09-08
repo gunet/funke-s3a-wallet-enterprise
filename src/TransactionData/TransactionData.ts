a954cd34acb2c288974dc71f81c267326d5a776e
Emmanouil Koukoularis <koukoularisman@gmail.com>
2025-09-08 16:59:28 +0300

    support transaction data qes

--- src/TransactionData/TransactionData.ts
+++ src/TransactionData/TransactionData.ts
@@ -2,6 +2,8 @@ import { base64url } from 'jose';
 import crypto from 'node:crypto';
 
 import { arrayBufferToBase64Url } from '../util/arrayBufferToBase64Url';
+import { fromBase64Url } from "wallet-common/dist/utils/util";
+
 
 export const ExampleTransactionData = () => {
 	const webcrypto = crypto.subtle;
@@ -33,10 +35,52 @@ export const ExampleTransactionData = () => {
 	}
 }
 
-export const TransactionData = (transactionDataType: 'urn:wwwallet:example_transaction_data_type') => {
+export const QESAuthorizationTransactionData = () => {
+	const webcrypto = crypto.subtle;
+
+	const generateTransactionDataRequestObject = async (descriptorId: string) => {
+		return base64url.encode(JSON.stringify({
+			type: 'qes_authorization',
+			credential_ids: [descriptorId],
+			signatureQualifier: "eu_eidas_qes",
+			transaction_data_hashes_alg: ["sha-256"],
+			documentDigests: [
+				{
+					hash: "some-hash-of-the-document",
+					label: "Terms & Conditions.pdf",
+					hashAlgorithmOID: "2.16.840.1.101.3.4.2.1"
+				}
+			],
+		}));
+	}
+
+	return {
+		generateTransactionDataRequestObject,
+
+		validateTransactionDataResponse: async (exprectedDescriptorId: string, params: { transaction_data_hashes: string[], transaction_data_hashes_alg?: string[] }) => {
+			const expectedObjectB64U = await generateTransactionDataRequestObject(exprectedDescriptorId);
+			const expectedObjectDecoded = fromBase64Url(expectedObjectB64U);
+			for (const hashB64U of params.transaction_data_hashes) {
+				if (!params.transaction_data_hashes_alg || params.transaction_data_hashes_alg.includes('sha-256')) { // sha256 case
+					const calculatedHashOfExpectedObject = arrayBufferToBase64Url(await webcrypto.digest('SHA-256', expectedObjectDecoded));
+					console.log("calculatedHash = ", calculatedHashOfExpectedObject);
+					console.log("hashB64U = ", hashB64U);
+					if (calculatedHashOfExpectedObject === hashB64U) {
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+	}
+}
+
+export const TransactionData = (transactionDataType: 'urn:wwwallet:example_transaction_data_type' | 'qes_authorization') => {
 	switch(transactionDataType) {
 		case "urn:wwwallet:example_transaction_data_type":
 			return ExampleTransactionData();
+		case "qes_authorization":
+			return QESAuthorizationTransactionData();
 		default:
 			return null;
 	}
