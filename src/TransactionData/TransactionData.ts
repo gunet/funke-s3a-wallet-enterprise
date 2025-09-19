fa70c9be6d0a040e1a9a97abb663d20a25727bae
Emmanouil Koukoularis <koukoularisman@gmail.com>
2025-09-19 18:37:12 +0300

    openid4vp: change trasaction data types for qes and qc request

--- src/TransactionData/TransactionData.ts
+++ src/TransactionData/TransactionData.ts
@@ -41,15 +41,17 @@ export const QESAuthorizationTransactionData = () => {
 
 	const generateTransactionDataRequestObject = async (descriptorId: string) => {
 		return base64url.encode(JSON.stringify({
-			type: 'qes_authorization',
+			type: 'https://cloudsignatureconsortium.org/2025/qes',
 			credential_ids: [descriptorId],
 			signatureQualifier: "eu_eidas_qes",
-			transaction_data_hashes_alg: ["sha-256"],
+			transaction_data_hashes_alg: "sha-256",
+			numSignatures: 1,
+			processID: "random-process-id",
 			documentDigests: [
 				{
 					hash: "some-hash-of-the-document",
-					label: "Terms & Conditions.pdf",
-					hashAlgorithmOID: "2.16.840.1.101.3.4.2.1"
+					label: "Example Contract",
+					hashType: "sodr"
 				}
 			],
 		}));
@@ -76,12 +78,49 @@ export const QESAuthorizationTransactionData = () => {
 	}
 }
 
-export const TransactionData = (transactionDataType: 'urn:wwwallet:example_transaction_data_type' | 'qes_authorization') => {
-	switch(transactionDataType) {
+export const QCRequestTransactionData = () => {
+	const webcrypto = crypto.subtle;
+
+	const generateTransactionDataRequestObject = async (descriptorId: string) => {
+		return base64url.encode(JSON.stringify({
+			type: 'https://cloudsignatureconsortium.org/2025/qc-request',
+			credential_ids: [descriptorId],
+			QC_terms_conditions_uri: "https://qtsp.example.com/policies/terms_and_conditions.pdf",
+			QC_hash: "ohxKcClPp/J1dI1iv5x519BpjduGZC794x4ABFeb+Ds=",
+			QC_hashAlgorithmOID: "2.16.840.1.101.3.4.2.1",
+			transaction_data_hashes_alg: "sha-256"
+		}));
+	}
+
+	return {
+		generateTransactionDataRequestObject,
+
+		validateTransactionDataResponse: async (exprectedDescriptorId: string, params: { transaction_data_hashes: string[], transaction_data_hashes_alg?: string[] }) => {
+			const expectedObjectB64U = await generateTransactionDataRequestObject(exprectedDescriptorId);
+			const expectedObjectDecoded = fromBase64Url(expectedObjectB64U);
+			for (const hashB64U of params.transaction_data_hashes) {
+				if (!params.transaction_data_hashes_alg || params.transaction_data_hashes_alg.includes('sha-256')) { // sha256 case
+					const calculatedHashOfExpectedObject = arrayBufferToBase64Url(await webcrypto.digest('SHA-256', expectedObjectDecoded));
+					console.log("calculatedHash = ", calculatedHashOfExpectedObject);
+					console.log("hashB64U = ", hashB64U);
+					if (calculatedHashOfExpectedObject === hashB64U) {
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+	}
+}
+
+export const TransactionData = (transactionDataType: 'urn:wwwallet:example_transaction_data_type' | 'https://cloudsignatureconsortium.org/2025/qes' | 'https://cloudsignatureconsortium.org/2025/qc-request') => {
+	switch (transactionDataType) {
 		case "urn:wwwallet:example_transaction_data_type":
 			return ExampleTransactionData();
-		case "qes_authorization":
+		case "https://cloudsignatureconsortium.org/2025/qes":
 			return QESAuthorizationTransactionData();
+		case "https://cloudsignatureconsortium.org/2025/qc-request":
+			return QCRequestTransactionData();
 		default:
 			return null;
 	}
